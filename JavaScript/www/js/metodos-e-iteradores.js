23afbda9306d4a34a7d8e93dcf92e190fc1c4810
sagrado13 <igonvaz@outlook.es>
2021-03-03 23:18:27 +0100

    métodos e iterables

---
+++ JavaScript/www/js/metodos-e-iteradores.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const persona = {
+  nombre: "Iñaki",
+  apellido: "González",
+  edad: 31,
+  id: 3,
+  informacionCompleta: function () {
+    return `${this.nombre} ${this.apellido} tiene ${this.edad} años`;
+  },
+};
+
+console.log(persona.informacionCompleta());
+
+const miArray = [1, 2, 3, "hola", 28, { a: 1 }];
+console.log(typeof miArray);
+
+let iterador = miArray[Symbol.iterator]();
+
+console.log(iterador.next().value);
+console.log(iterador.next().value);
+console.log(iterador.next().value);
+console.log(iterador.next().value);
+console.log(iterador.next().value);
+console.log(iterador.next().value);
