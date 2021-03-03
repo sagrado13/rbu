8f80190cfc5ca10dcb99647db88a070b181c144f
sagrado13 <igonvaz@outlook.es>
2021-03-03 14:42:53 +0100

    JavaScript básico

---
+++ JavaScript/www/js/JSON-objeto-window.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/* JSON = JavaScript Object Notation */
+let persona = {
+  nombre: "Iñaki",
+  apellido: "González",
+  edad: 31,
+};
+
+console.log(persona);
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.edad);
+
+/* OBJECTO WINDOW */
+
+console.log(window);
+
+console.log(window.document);
+console.log(window.history);
+console.log(window.navigator);
+console.log(window.screen);
+console.log(window.visualViewport);
+console.log(window.screenX);
+console.log(window.screenY);
+console.log(window.innerWidth);
+console.log(innerWidth);
+
+/* alert("hello world");
+window.alert("hello world"); */
+
+/* DOCUMENT */
+
+console.log(document);
+console.dir(document);
+let miArray = [1, 2, 3];
+let miSet = new Set(miArray);
+console.log(miSet);
---
+++ JavaScript/www/js/objeto-null-undefined.js
@@ -0,0 +1,41 @@
+"use stricit";
+
+/* STRING */
+let string1 = "texto";
+console.log(typeof string1);
+
+/* NUMBER */
+let number1 = 1;
+console.log(typeof number1);
+
+/* OBJECT */
+let objeto1 = {
+  propiedad1: "texto1",
+  prop2: 1,
+  prop3: {},
+};
+
+console.log(objeto1);
+console.log(typeof objeto1);
+
+console.log(objeto1.propiedad1);
+console.log(typeof objeto1.propiedad1);
+
+console.log(objeto1.prop2);
+console.log(typeof objeto1.prop2);
+
+console.log(objeto1.prop3);
+console.log(typeof objeto1.prop3);
+
+/* NULL */
+console.log(null);
+console.log(typeof null);
+
+/* UNDEFINED */
+console.log(undefined);
+console.log(typeof undefined);
+
+/* BOOLEAN */
+let uno = true;
+let dos = false;
+console.log(typeof uno, typeof dos);
